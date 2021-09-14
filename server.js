8540854ac0a555b4342758ceb771257e1e982102
Drish-xD <drish.xd@gmail.com>
2021-09-14 11:42:14 +0530

    Heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 
-const app = express();
+app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port);
