2a5dd00111c8893076723c6ae4e0157cb2569075
Drish-xD <Drish.xD@gmail.com>
2021-11-07 01:37:14 +0530

    update  deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Drish-xD/vue-website.git master:gh-pages
+git push -f https://github.com/Drish-xD/vue-website.git master:gh-pages
 
 cd -
