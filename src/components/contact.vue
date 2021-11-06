84f2b7f9ba378a714c3f4577ca74c450c41c3946
Drish-xD <Drish.xD@gmail.com>
2021-11-07 01:14:59 +0530

    tesla V1

---
+++ src/components/contact.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="bg" id="contact">
+    <h1>Contacts</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'contacts',
+}
+</script>
+
+<style scoped>
+.bg {
+  /* background: url("../../src/assets/img/Tesla.jpg") no-repeat center center/cover; */
+  width: 100%;
+  height: 10vh;
+}
+</style>
